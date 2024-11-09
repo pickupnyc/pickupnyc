e428b0a8fc90857fca9034ed12858813dbf0f3e4
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-11-08 22:25:17 -0500

    style: restyle pages and components with extended Tailwind colors

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,16 +9,33 @@ const Header = () => {
     const { user } = useUser();
 
     return (
-        <div className="flex w-full flex-row items-center justify-between border-b border-gray-200 px-24 py-6 bg-red-500">
+        <div className="mx-auto flex h-20 w-full max-w-[1440px] flex-row items-center justify-between px-24 py-6 md:h-24">
             <div className="space-x-8">
-                <Link to="/" className="text-black font-bold">Home</Link>
-                <Link to="/matches" className="text-black font-bold">Forum</Link>
+                <Link to="/">
+                    <span className="text-primary text-3xl font-bold">PickUpNYC</span>
+                </Link>
             </div>
-            <div className="flex flex-row items-center justify-center">
+            <div className="flex flex-row gap-x-8">
+                <Link to="/" className="hover:bg-tea_green flex items-center rounded-full px-5 py-3">
+                    <span className="text-primary font-bold tracking-wide">Home</span>
+                </Link>
+                <Link to="/matches" className="hover:bg-tea_green flex items-center rounded-full px-5 py-3">
+                    <span className="text-primary font-bold tracking-wide">Matches</span>
+                </Link>
+                <Link to="/forum" className="hover:bg-tea_green flex items-center rounded-full px-5 py-3">
+                    <span className="text-primary font-bold tracking-wide">Forum</span>
+                </Link>
+                <Link to="/help" className="hover:bg-tea_green flex items-center rounded-full px-5 py-3">
+                    <span className="text-primary font-bold tracking-wide">Help</span>
+                </Link>
+            </div>
+            <div className="flex flex-row">
                 {user ? <LogoutButton /> : <LoginButton />}
-                {user  && (
+                {/* {user && (
                     <img
-                        src={'https://www.bing.com/ck/a?!&&p=d5c65e5da2ba3509a025e943e7942fb9c32759e9f3d4ef816073b732e1de37a4JmltdHM9MTczMDQxOTIwMA&ptn=3&ver=2&hsh=4&fclid=3fa412f8-9676-6bfa-1a78-07d9975f6a3a&u=a1L2ltYWdlcy9zZWFyY2g_cT1hdmF0YXIlMjBpY29uJkZPUk09SVFGUkJBJmlkPTk3Q0UyRUQ1QzlCOTRDOTgyN0U3RDUxNENDNDQ1RUI0MkVBODYwMTU&ntb=1'}
+                        src={
+                            "https://www.bing.com/ck/a?!&&p=d5c65e5da2ba3509a025e943e7942fb9c32759e9f3d4ef816073b732e1de37a4JmltdHM9MTczMDQxOTIwMA&ptn=3&ver=2&hsh=4&fclid=3fa412f8-9676-6bfa-1a78-07d9975f6a3a&u=a1L2ltYWdlcy9zZWFyY2g_cT1hdmF0YXIlMjBpY29uJkZPUk09SVFGUkJBJmlkPTk3Q0UyRUQ1QzlCOTRDOTgyN0U3RDUxNENDNDQ1RUI0MkVBODYwMTU&ntb=1"
+                        }
                         alt="User Avatar"
                         className="ml-4 h-10 w-10 rounded-full"
                         onError={(e) => {
@@ -26,7 +43,7 @@ const Header = () => {
                             e.target.src = `https://ui-avatars.com/api/?name=${user.username}`;
                         }}
                     />
-                )}
+                )} */}
             </div>
         </div>
     );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,51 +8,54 @@ const Login = () => {
 
     const toggle = () => {
         setPassVisible(!passVisible);
-    }
+    };
     return (
         <div className="flex h-screen w-full items-center justify-center">
-            
-            <main className="flex flex-col  h-screen w-full items-center justify-center relative bg-purple-50">
-                <div className="absolute inset-44 container mx-auto border-2 bg-white md:w-2/5 md:h-3/5 lg:w-80 lg:h-2/5 xl:w-1/4 xl:h-1/2 rounded-lg shadow-xl">
-                    <div className="flex flex-col justify-center items-center">
-                        <h1 className="text-center text-2xl font-semibold p-3 pb-1 mb-1">Sign In</h1>
-                        <p className="mb-2">Welcome to <span className="text-blue-500 inline font-bold">PickUp NYC.</span></p>
-                    
+            <main className="bg-offWhite relative flex h-screen w-full flex-col items-center justify-center">
+                <div className="container absolute inset-44 mx-auto rounded-lg border-2 bg-white shadow-xl md:h-3/5 md:w-2/5 lg:h-2/5 lg:w-80 xl:h-1/2 xl:w-1/4">
+                    <div className="flex flex-col items-center justify-center">
+                        <h1 className="mb-1 p-3 pb-1 text-center text-2xl font-semibold">Sign In</h1>
+                        <p className="mb-2">
+                            Welcome to <span className="inline font-bold text-blue-500">PickUp NYC.</span>
+                        </p>
+
                         <div className="mb-4">
-                            <label htmlFor="username" className="block text-gray-800 font-bold text-sm">
+                            <label htmlFor="username" className="block text-sm font-bold text-gray-800">
                                 Email:
                             </label>
                             <input
                                 placeholder="Example@gmail.com"
                                 type="email"
                                 name="username"
-                                className="block w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
+                                className="block w-56 rounded-md px-2 py-1.5 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
                             />
                         </div>
 
-                        <div className="mb-4 relative">
-                            <label htmlFor="passwd" className="block text-gray-800 font-semibold text-sm">
+                        <div className="relative mb-4">
+                            <label htmlFor="passwd" className="block text-sm font-semibold text-gray-800">
                                 Password:
                             </label>
                             <input
                                 placeholder="Password"
-                                type={ (passVisible === false)? "password": "text"}
+                                type={passVisible === false ? "password" : "text"}
                                 name="passwd"
-                                className="block w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
+                                className="block w-56 rounded-md px-2 py-1.5 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
                             />
 
-                            <div className="text-xl absolute top-7 right-5">
-                                {
-                                    (passVisible === false)? <FaEyeSlash onClick={toggle}/>:
-                                    <FaEye onClick={toggle} />
-                                }
+                            <div className="absolute right-5 top-7 text-xl">
+                                {passVisible === false ? <FaEyeSlash onClick={toggle} /> : <FaEye onClick={toggle} />}
                             </div>
                         </div>
 
-                        <Link to="" className="mb-4">Forgot Password?</Link>
+                        <Link to="" className="mb-4">
+                            Forgot Password?
+                        </Link>
 
                         <div className="mb-4">
-                            <Link to="" className="cursor-pointer bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-10 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150" >
+                            <Link
+                                to=""
+                                className="mt-4 cursor-pointer rounded-md bg-gradient-to-r from-indigo-500 to-blue-500 px-10 py-2 font-bold text-white transition duration-150 ease-in-out hover:bg-indigo-600 hover:to-blue-600"
+                            >
                                 Sign In
                             </Link>
                         </div>
@@ -62,9 +65,13 @@ const Login = () => {
                         </div>
 
                         <div className="">
-                            <p className="">Dont have an account? <Link to="/signup"><strong>Sign Up</strong></Link></p>
+                            <p className="">
+                                Dont have an account?{" "}
+                                <Link to="/signup">
+                                    <strong>Sign Up</strong>
+                                </Link>
+                            </p>
                         </div>
-                        
                     </div>
                 </div>
             </main>
