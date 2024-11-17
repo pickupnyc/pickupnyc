5489d9ee75ae9bdde4c0b9570511fda4b8019048
jawadrada <jawadrada18@gmail.com>
2024-11-16 19:44:14 -0500

    Update footer styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,12 @@
 function Footer() {
     return (
         <footer className="bg-darkGreen py-6 text-center">
-            <p className="text-offWhite">&copy; 2024 PickUpNYC</p>
+            <p className="text-offWhite text-sm md:text-base lg:text-lg">
+                &copy; 2024 <strong>PickUpNYC.</strong> All rights reserved.
+            </p>
         </footer>
     );
 }
 
 export default Footer;
+
