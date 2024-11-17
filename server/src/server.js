3067255c33c0b91dc2d50b2facd27c5a7e8d9422
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-11-17 18:01:00 -0500

    add trust proxy after changing redirect_uri

--- server/src/server.js
+++ server/src/server.js
@@ -26,6 +26,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("public"));
 }
 
+app.set("trust proxy", 1); 
 app.use(
     session({
         secret: process.env.COOKIE_SECRET,
