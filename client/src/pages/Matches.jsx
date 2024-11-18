0a9dddfa5b8053eb83ce2f28dd8881f5264094b5
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-11-17 22:41:13 -0500

    add error for unauthenticated user trying to create a match

--- client/src/pages/Matches.jsx
+++ client/src/pages/Matches.jsx
@@ -8,10 +8,12 @@ import MatchForm from "../components/MatchForm";
 const Matches = () => {
     const { user } = useUser();
     const [isOpen, setIsOpen] = useState(false);
+    const [showAuthError, setShowAuthError] = useState(false);
 
     const handleOpenForm = () => {
         if (!user) {
-            console.error("Must be authenticated!");
+            setShowAuthError(true);
+            setTimeout(() => setShowAuthError(false), 3000);
         } else {
             setIsOpen(true);
         }
@@ -23,13 +25,18 @@ const Matches = () => {
 
     return (
         <div className="mx-auto flex w-full max-w-[1440px] flex-col px-24 py-8">
-            <div className="flex w-full items-center justify-center">
+            <div className="relative flex w-full flex-col items-center justify-center gap-y-4 rounded-xl bg-gray-100 p-4">
                 <button
                     onClick={handleOpenForm}
                     className="flex max-w-[140px] justify-center rounded-full border-2 border-darkGreen bg-darkGreen px-6 py-4 text-offWhite hover:bg-offWhite hover:text-darkGreen"
                 >
                     Create +
                 </button>
+                {showAuthError && (
+                    <div className="absolute -bottom-10 rounded-md bg-red-500 px-4 py-2 text-white">
+                        Only valid users can create a match!
+                    </div>
+                )}
             </div>
 
             <Modal isOpen={isOpen} onClose={handleCloseForm}>
