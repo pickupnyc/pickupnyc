e4ff722e8da94e4ca509c892236b997f45df939a
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-11-18 20:23:54 -0500

    set up routes and pages for editing and viewing match info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,10 @@ import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import Header from "./components/Header";
 import Matches from "./pages/Matches";
+import MatchInfo from "./pages/MatchInfo";
 import Help from "./pages/Help";
 import Forum from "./pages/Forum";
+import EditMatch from "./pages/EditMatch";
 
 function App() {
     return (
@@ -23,11 +25,13 @@ function App() {
                             <Routes>
                                 <Route path="/" element={<Home />} />
                                 <Route path="/signup" element={<Signup />} />
-                                <Route path="*" element={<Fallback />} />
                                 <Route path="/login" element={<Login />} />
                                 <Route path="/matches" element={<Matches />} />
+                                <Route path="/matches/:id" element={<MatchInfo />} />
+                                <Route path="/matches/edit/:id" element={<EditMatch />} />
                                 <Route path="/forum" element={<Forum />} />
                                 <Route path="/help" element={<Help />} />
+                                <Route path="*" element={<Fallback />} />
                             </Routes>
                         </div>
                         <Footer />
